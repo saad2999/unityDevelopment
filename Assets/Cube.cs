70540acd3e37cab56d50af76b2b89e098208c83f
DESKTOP-DVQ765F\Maaz <saad586305@gmail.com>
2022-09-06 17:53:52 +0500

    add a cube

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -7,7 +7,8 @@ public class Cube : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("i am printing from debug");
+        Debug.Log("i am saad");
     }
 
     // Update is called once per frame
