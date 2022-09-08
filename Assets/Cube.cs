b8204159e4ab22c5714483d3edf33d0f9ff86af9
DESKTOP-DVQ765F\Maaz <saad586305@gmail.com>
2022-09-08 21:09:45 +0500

    make my cube move in four directions

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Rigidbody2D Myrigidbody2D;
     void Start()
     {
       //  Debug.Log("i am printing from debug");
@@ -19,14 +20,29 @@ public class Cube : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.UpArrow))
         {
             Debug.Log("up key is pressed");
-            transform.position += new Vector3(1, 1, 0);
+            Myrigidbody2D.velocity = new Vector2(0f, 10f);
             
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             Debug.Log("down key is pressed");
-            transform.position+= new Vector3(-1, -1, 0);
-            
+            Myrigidbody2D.velocity = new Vector2(0f, -10f);
+
+
+
+        }if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log("right key is pressed");
+            Myrigidbody2D.velocity = new Vector2(10f, 0f);
+
+
+
+        }if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log("left key is pressed");
+            Myrigidbody2D.velocity = new Vector2(-10f, 0f);
+
+
 
         }
 
