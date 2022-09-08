07faf192a2b5e701b7cc6e454392fb541457680b
DESKTOP-DVQ765F\Maaz <saad586305@gmail.com>
2022-09-08 18:32:40 +0500

    fun with colider

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -7,15 +7,27 @@ public class Cube : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("i am printing from debug");
-        Debug.Log("i am saad");
-        Debug.LogWarning("I am a warning ignore me anyway ");
-        Debug.LogError("I am error, I can make your life hell");
+      //  Debug.Log("i am printing from debug");
+       // Debug.Log("i am saad");
+        //Debug.LogWarning("I am a warning ignore me anyway ");
+       // Debug.LogError("I am error, I can make your life hell");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("up key is pressed");
+            transform.position += new Vector3(0, 1, 0);
+            
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("down key is pressed");
+            transform.position+= new Vector3(0, -1, 0);
+
+        }
+
     }
 }
